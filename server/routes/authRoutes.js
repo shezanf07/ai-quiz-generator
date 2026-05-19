c484ae2104b2aad4d80f3989121fc7284b121ef6
Shezan Fayaz <shezanf7@gmail.com>
2026-05-19 14:53:10 +0530

    Improved

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { register, login } from '../controllers/authController.js';
+import { register, login , googleLogin ,getConfig} from '../controllers/authController.js';
 
 const router = express.Router();
 
